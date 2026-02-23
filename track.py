a1b4331a24cfdfedd6e115f3a14f856b6b5d9aff
Tyopo131 <samecob62@gmail.com>
2026-02-23 20:01:05 +0000

    Add simple saving system

--- track.py
+++ track.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import shlex
 from typing import Self
+import json
 
 class Cancel(Exception):
     pass
@@ -26,7 +27,7 @@ done: exit the resolver
                 break
             case "-":
                 if len(command) < 2:
-                    printf("- requires 1 argument")
+                    print("- requires 1 argument")
                     continue
                 if not command[1].isnumeric():
                     print("amount must be an int")
@@ -127,16 +128,95 @@ class Phys(Bank):
 bank = Bank() # SENTINEL
 phys = Phys() # SENTINEL
 
-accounts: dict[Account] = {}
+accounts: dict[str, Account] = {}
 
+currentFilename: str | None = None
+needsSave = False
 running: bool = True
 
 prompt: str = "--> "
+
+def save(filename: str):
+    global currentFilename
+    global accounts
+    global needsSave
+    try:
+        with open(filename, "wt") as f:
+            specials: dict[str, str] = {}
+            accs: dict[str, int] = {}
+            for name, value in accounts.items():
+                if value is phys:
+                    specials[name] = "phys"
+                elif value is bank:
+                    specials[name] = "bank"
+                else:
+                    accs[name] = value.money
+            combined = {
+                "specials": specials,
+                "accounts": accs
+            }
+            json.dump(combined, f)
+            currentFilename = filename
+            needsSave = False
+                    
+    except Exception as e:
+        print(f"saving failed: {e}")
+        raise Cancel("error while saving")
+def load(filename: str, wipe: bool=True, skipConfirm: bool=False):
+    global accounts
+    global needsSave
+
+    if wipe and not skipConfirm:
+        while (True):
+            confirmation = input("This will wipe all existing data! Are you sure? (y/n): ")
+            match confirmation:
+                case "y": break
+                case "n": raise Cancel("User denied wipe confirmation")
+                case _: continue
+    try:
+        with open(filename, "rt") as f:
+            combined = json.load(f)
+            if wipe:
+                accounts = {}
+            specials = combined["specials"]
+            accs = combined["accounts"]
+            for name, value in specials.items():
+                if value == "phys":
+                    accounts[name] = phys
+                elif value == "bank":
+                    accounts[name] = bank
+                else:
+                    print(f'loading {filename} warning: unknown special type \'{value}\' for account \'{name}\'')
+                    continue
+            for name, money in accs.items():
+                acc = Account()
+                bank.transfer(acc, money)
+                accounts[name] = acc
+            currentFilename = filename
+            needsSave = False
+
+    except Exception as e:
+        print(f"loading failed: {e}")
+        raise Cancel("cancelled: error while loading")
+def saveCheck() -> None:
+    global needsSave
+    if needsSave:
+        while True:
+            confirmation = input("There is unsaved work! Continue anyway? (y/n): ")
+            match confirmation.lower():
+                case "y":
+                    break
+                case "n":
+                    raise Cancel("Data was not saved and user did not confirm")
+                case _:
+                    continue
 def matchCommands(command: list[str]):
     global running
     global bank
     global phys
     global accounts
+    global currentFilename
+    global needsSave
     match command[0]:
         case "newAcc":
             if len(command) < 2:
@@ -150,6 +230,7 @@ def matchCommands(command: list[str]):
                     return
                 bank.transfer(account, int(amount))
             accounts[command[1]] = account
+            needsSave = True
             
         case "getAccs":
             for accName, account in accounts.items():
@@ -159,11 +240,13 @@ def matchCommands(command: list[str]):
                 print("usage: bank [accName]")
                 return
             accounts[command[1]] = bank
+            needsSave = True
         case "phys":
             if len(command) < 2:
                 print("usage: phys [accName]")
                 return
             accounts[command[1]] = phys
+            needsSave = True
         case "pay" | "transfer":
             if len(command) < 4:
                 print("usage: pay [acc1] [acc2] [amount]")
@@ -182,7 +265,27 @@ def matchCommands(command: list[str]):
                 return
             transferAccount: Account = accounts[acc1]
             transferAccount.transfer(accounts[acc2], int(amount))
+            needsSave = True
+        case "save":
+            if len(command) >= 2:
+                save(command[1])
+            else:
+                if currentFilename is None:
+                    print("no current file and no filename specified, usage: save [filename]")
+                    return
+                save(currentFilename)
+        case "load":
+            if len(command) < 2:
+                print("usage: load <filename>")
+            saveCheck()
+            load(command[1], wipe=True, skipConfirm=False)
+        case "loada":
+            if len(command) < 2:
+                print("usage: loada <filename>")
+            load(command[1], wipe=False)
+            needsSave = True
         case "exit":
+            saveCheck()
             running = False
         case _:
             print(f"command not found: '{command[0]}'")
